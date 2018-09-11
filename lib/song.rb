7df069a3ba84d22b1184666ae172d0bcfb7c2e77
Gabriela Suriano <surianogabriela@gmail.com>
2018-09-11 10:49:07 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,20 @@
 class Song 
-  attr_accesor :name, :artist, :assert_generates
+  attr_accesor :name, :artist, :genre 
+  
+  @@count = 0 
+  @@genres = [] 
+  @@artists - [] 
+  
+  def initialize(song_name, artist, genre)
+    @name = song_name
+    @artist = artist
+    @genre = genre
+    @@count += 1 
+    @@genres << genres
+    @@artists << artist 
+  end 
+  
+  def self.count 
+    @@count
+  
 end 
